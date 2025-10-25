65e8f3fb8a7f90a647bb4eaf12fd1e036aea86ca
brian <bsipin@gmail.com>
2025-10-25 16:02:10 -0400

    fix

---
+++ bootstrap/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.6.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "staging_backend" {
+  source = "../modules/opentofu-backend"
+
+  environment          = "staging"
+  bucket_name          = "staging-terraform-state-12093"
+}
+
