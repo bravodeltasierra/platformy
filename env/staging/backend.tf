9a19281b980ce2ba132a4cf21a9b5a52e5cc2d13
brian <bsipin@gmail.com>
2025-10-25 16:08:16 -0400

    lint

--- env/staging/backend.tf
+++ env/staging/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "staging-terraform-state-12093"
-    key            = "staging/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
+    bucket  = "staging-terraform-state-12093"
+    key     = "staging/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
   }
 }
 
