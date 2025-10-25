29d1a9cf639044758a68fb5dc67cb7493e009bc2
brian <bsipin@gmail.com>
2025-10-25 15:54:19 -0400

    fix

--- modules/opentofu-backend/main.tf
+++ modules/opentofu-backend/main.tf
@@ -1,24 +1,22 @@
 resource "aws_s3_bucket" "opentofu_state" {
   bucket = var.bucket_name
-
+  
   lifecycle {
     prevent_destroy = true
   }
-
-  tags = var.tags,
 }
 
-resource "aws_s3_bucket_versioning" "opentofu_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
+resource "aws_s3_bucket_versioning" "opentofu_bucket_versioning" {
+  bucket = aws_s3_bucket.opentofu_state.id
+  
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "opentofu_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
+resource "aws_s3_bucket_server_side_encryption_configuration" "opentofu_config" {
+  bucket = aws_s3_bucket.opentofu_state.id
+  
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -26,25 +24,24 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "opentofu_state" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
+resource "aws_s3_bucket_public_access_block" "opentofu_access" {
+  bucket = aws_s3_bucket.opentofu_state.id
+  
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
+resource "aws_s3_bucket_lifecycle_configuration" "opentofu_lifestyle_config" {
+  bucket = aws_s3_bucket.opentofu_state.id
+  
   rule {
     id     = "delete-old-versions"
     status = "Enabled"
-
+    
     noncurrent_version_expiration {
-      noncurrent_days = var.old_version_expiration_days
+      noncurrent_days = 90
     }
   }
 }
-
